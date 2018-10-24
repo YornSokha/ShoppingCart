4f2b4e9bdf5ec01aa035b2f9b818b9cdc00706a2
SokhaYorn <yornsokha168@gmail.com>
2018-10-25 00:13:19 +0700

    Change something

--- src/kh/edu/ckcc/assignment/classes/Main.java
+++ src/kh/edu/ckcc/assignment/classes/Main.java
@@ -27,142 +27,147 @@ public class Main {
 		while(!quit) {
 			option = menu();
 			switch(option) {
-			case "1":
-					createNewProduct();
-				break;
-				
-			case "2":
-					showProductList();
-				break;
-				
-			case "3":
-					shoppingProduct(cart);
-				break;
-			case "4":
-				String key;
-				if(!cart.getPurchasedItems().isEmpty()) {
-					do {
-						showMyShoppingCart(cart);
-						System.out.println("	1. Remove item");
-						System.out.println("	2. Add more item(s)");
-						System.out.println("	3. Back to Main Menu");
-						key = scanner.nextLine();
-						switch(key) {
-						case "1":
-							System.out.println("Input purchase No to remove : "); String purNo = scanner.next();
-								for(Purchase pur : cart.getPurchasedItems()) {
-									if(pur.getOrderNo().equals(purNo)) {
-											//cart.removeItem(pur);		
-										Purchase cancelledPurchase = pur;
-				                        for(int j = 0; j < products.size(); j++) {				                           
-				                                Product product = products.get(j);
-				                                if((product.getID()).equals(cancelledPurchase.getProduct().getID())){
-				                                    products.set(j, new Product(
-				                                            product.getID(), product.getName(),
-				                                            product.getDescription(),
-				                                            product.getPrice(),
-				                                            product.getQtyInStock() + cancelledPurchase.getQty()));
-				                                }
-				                                
-				                        }
-				                        cart.removeItem(pur);				         
-				                        System.out.println("You products have been removeed!");
-				                        break;
+				case "1":
+						createNewProduct();
+					break;
+					
+				case "2":
+						showProductList();
+					break;
+					
+				case "3":
+						shoppingProduct(cart);
+					break;
+				case "4":
+					String key;
+					boolean back = false;
+					if(!cart.getPurchasedItems().isEmpty()) {
+						do {
+							showMyShoppingCart(cart);
+							System.out.println("	1. Add more purchase");
+							System.out.println("	2. Remove purchase");
+							System.out.println("	3. Back");
+							key = scanner.nextLine();
+							switch(key) {
+							case "1":
+								System.out.println("---------------------Shopping more product(s)---------------------");
+								shoppingProduct(cart);
+								break;
+								
+							case "2":
+								System.out.println("Input purchaseNo to remove : "); String purNo = scanner.nextLine();
+								boolean exist = false;
+									for(Purchase pur : cart.getPurchasedItems()) {
+										if(pur.getOrderNo().equals(purNo)) {
+												//cart.removeItem(pur);		
+											Purchase cancelledPurchase = pur;
+					                        for(int j = 0; j < products.size(); j++) {				                           
+					                                Product product = products.get(j);
+					                                if((product.getID()).equals(cancelledPurchase.getProduct().getID())){
+					                                    products.set(j, new Product(
+					                                            product.getID(), product.getName(),
+					                                            product.getDescription(),
+					                                            product.getQtyInStock() + cancelledPurchase.getQty(),
+					                                            product.getPrice()));
+					                                }
+					                                
+					                        }
+					                        cart.removeItem(pur);				         
+					                        System.out.println("You products have been removed!");
+					                        exist = true;
+					                        break;
+										}
+										
 									}
-									else {
+									if(!exist){
 										System.out.println("Invalid product name or Order ID");
 									}
-								}
-
+								
+								break;
+							case "3":
+								back = true;
+								break;
+							}
 							
+						}while(!back);
+					}	
+					else {
+						System.out.println("Sorry, you haven't shopped any product yet\nPlease go to buy our product first");	
+						scanner.nextLine();
+					}
+					break;
+				case "5":
+					String ch;
+					Customer customer = null;	
+					do {
+						System.out.println("	1. Add new Customer to check out");
+						System.out.println("	2. Cancel Order");
+						System.out.println("	3. Go back to Main Menu");
+						ch = scanner.nextLine();
+						switch(ch) {
+						case "1":
+							if(!cart.getPurchasedItems().isEmpty()) {
+								System.out.println("-------------------------------Before Check out, give me your information-------------------------------");
+								System.out.println("Enter your identification Number : "); String id = scanner.nextLine();
+								System.out.println("Enter your name : "); String name = scanner.nextLine();
+								System.out.println("Enter your email : "); String email = scanner.nextLine();
+								System.out.println("Enter your shipping address : "); String shippingAddress = scanner.nextLine();
+								System.out.println("Enter your billing address :  "); String billingAddress = scanner.nextLine();
+								System.out.println("\n-------------------------------Do you have discount card?---------------------------------------------");
+								System.out.println("\nEnter Pertage on your discount card : "); double discountCard = scanner.nextDouble();
+								//
+								cart.setDiscount(discountCard);
+								customer = new Customer(id, name, email, shippingAddress, billingAddress);
+								customer.placeOrder(cart);
+								customers.add(customer);
+								
+								System.out.println("\n------------------------------------Your purchased Invoice----------------------------------------------");
+								
+								showCustomer(customer);
+								cart = new Cart();
+								System.out.println("Arigato!!!!");
+								scanner.nextLine();
+							}
+							else {
+								System.out.println("Please go to shopping frist!!!");
+								scanner.nextLine();
+							}
 							break;
 						case "2":
-							System.out.println("---------------------Shopping more product(s)---------------------");
-								shoppingProduct(cart);
+							if(!cart.getPurchasedItems().isEmpty()) {
+								//customer.cancelOrder();
+								customer = new Customer();
+								System.out.println("Cart was completely removed!!!");
+							}
+							else {
+								System.out.println("There is no product in cart...");
+							}
+							scanner.nextLine();
 							break;
 						}
-					}while(!key.equalsIgnoreCase("3"));
-				}	
-				else {
-					System.out.println("Sorry, you haven't shopped any product yet\nPlease go to buy our product first");	
-					scanner.nextLine();
-				}
-				break;
-			case "5":
-				String ch;
-				Customer customer = null;	
-				do {
-					System.out.println("	1. Add new Customer to check out");
-					System.out.println("	2. Cancel Order");
-					System.out.println("	3. View Shopping History");
-					System.out.println("	4. Go back to Main Menu");
-					ch = scanner.nextLine();
-					switch(ch) {
-					case "1":
-						if(!cart.getPurchasedItems().isEmpty()) {
-							System.out.println("-------------------------------Before Check out, give me your information-------------------------------");
-							System.out.println("Enter your identification Number : "); String id = scanner.nextLine();
-							System.out.println("Enter your name : "); String name = scanner.nextLine();
-							System.out.println("Enter your email : "); String email = scanner.nextLine();
-							System.out.println("Enter your shipping address : "); String shippingAddress = scanner.nextLine();
-							System.out.println("Enter your billing address :  "); String billingAddress = scanner.nextLine();
-							System.out.println("\n-------------------------------Do you have discount card?---------------------------------------------");
-							System.out.println("\nEnter Pertage on your discount card : "); double discountCard = scanner.nextDouble();
-							//
-							customer = new Customer(id, name, email, shippingAddress, billingAddress);
-							customer.placeOrder(cart);
-							customer.getCart().setDiscount(discountCard);
-							customers.add(customer);
-							
-							System.out.println("\n------------------------------------Your purchased Invoice----------------------------------------------");
-							
-							showCustomer(customer);
-							cart = new Cart();
-							System.out.println("Arigato!!!!");
-							scanner.nextLine();
-						}
-						else {
-							System.out.println("Please go to shopping frist!!!");
-							scanner.nextLine();
-						}
-						break;
-					case "2":
-						if(!cart.getPurchasedItems().isEmpty()) {
-							//customer.cancelOrder();
-							customer = new Customer();
-							System.out.println("Cart was completely removed!!!");
-						}
-						else {
-							System.out.println("There is no product in cart...");
-						}
-						
-						scanner.nextLine();
-
-						break;
-					case "3":
-						String st;
-						if(!customers.isEmpty()) {
-							do {
-								showShoppingHistory();
-								System.out.println("1. Search Customer by customer ID");
-								System.out.println("2. Go back");
-								st = scanner.nextLine();
-								switch(st) {
+					}while(!ch.equals("3"));				
+					break;				
+				case "6":
+					String st;
+					if(!customers.isEmpty()) {
+						do {
+							showShoppingHistory();
+							System.out.println("1. Search Customer's invoice");
+							System.out.println("2. Go back");
+							st = scanner.nextLine();
+							switch(st) {
 								case "1":
-									System.out.println("Enter Customer ID : "); String cusID = scanner.nextLine();
+									System.out.println("Enter Customer ID : "); 
+									String cusID = scanner.nextLine();
 									searchCustomer(cusID);	
 									break;
-								}
-							}while(!st.equalsIgnoreCase("2"));
-						}
-						else {
-							System.out.println("No any customers to view!!!");
-						}
-						break;
+							}
+						}while(!st.equals("2"));
 					}
-				}while(!ch.equals("4"));				
-				break;
-					
+					else {
+						System.out.println("No any customers to view!!!");
+					}
+					break;
 			}
 			System.out.print("Do you want to continue to Main Menu? (Y/N)\n");
 //            if(scanner.next().equalsIgnoreCase("Y"))
@@ -186,6 +191,7 @@ public class Main {
 		drawingTable1.addRow("   3. Go to Shopping    ");
 		drawingTable1.addRow("   4. My Shopping Cart   ");
 		drawingTable1.addRow("   5. Check Out  ");
+		drawingTable1.addRow("   6. View Shopping History");
 	
 		drawingTable1.print();
 		System.out.print("Enter your choice : ");
@@ -204,7 +210,7 @@ public class Main {
 		 do{
              System.out.println("----------------- Create New Product --------------------\n");
              System.out.print("Enter your product code:\n");
-             scanner.next();
+//             scanner.nextLine();
              String code = scanner.nextLine();
              Product p = findProduct(code);
              if(p == null) {
@@ -223,8 +229,8 @@ public class Main {
                  System.out.println("Product code : " + code + " is already existed!");
              System.out.println("---------------------------------------------------\n" +
                      "Do you want to add more product? (Y/N)");
-             scanner.next();
-         }while(scanner.next().equalsIgnoreCase("Y"));
+             scanner.nextLine();
+         }while(scanner.nextLine().equalsIgnoreCase("Y"));
       
 	}
 	
@@ -238,7 +244,7 @@ public class Main {
 		drawingTable = new DrawingTable();
 		drawingTable.setHeaders(" ID ", " Name ", " QTY ", " Price ", " Description ");
 		for(Product p : products) {
-			drawingTable.addRow(p.getID() , p.getName(), p.getQtyInStock() + "", p.getPrice() + "", p.getDescription());		
+			drawingTable.addRow(p.getID() , p.getName(), p.getQtyInStock() + "", "$ " +  p.getPrice() + "", p.getDescription());		
 			}
 		drawingTable.print();
 	}
@@ -269,9 +275,9 @@ public class Main {
 		drawingTable.setHeaders(" NO ", " Name ", " Qty ", "Unit Price", " Discount ", " Price " );
 		
 		for(Purchase item : cart.getPurchasedItems()) {
-			drawingTable.addRow(item.getOrderNo().toString(), item.getProductName(), 
-					item.getQty() + "","$ " +item.getProductPrice(), item.getDiscount() + "",
-					item.getPrice() + "");
+			drawingTable.addRow(item.getOrderNo(), item.getProductName(), 
+					item.getQty() + "","$ " + item.getProductPrice(), (item.getDiscount() * 100) + "",
+					"$ " + item.getPrice() + "");
 		}
 		drawingTable.print();
 	}
@@ -290,7 +296,7 @@ public class Main {
 	public static void searchCustomer(String customerID) {
 		boolean i = false;
 		for(Customer customer : customers) {
-			if(customer.getCustomerID().equalsIgnoreCase(customerID)) {
+			if(customer.getCustomerID().equals(customerID)) {
 				showCustomer(customer);
 				i = true;
 			}
